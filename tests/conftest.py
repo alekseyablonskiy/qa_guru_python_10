8d21cf02dd165dd46048d2744e268e7b25209d30
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2023-01-20 15:26:37 +0300

    python-dotenv added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,10 @@
-import os
-
 import pytest
+import os
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
 from dotenv import load_dotenv
-
 from demoqa_tests.utils import attach
-
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -23,32 +12,31 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+@pytest.fixture(scope="function", autouse=True)
+def open_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "99.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
-    options.capabilities.update(selenoid_capabilities)
 
+    options.capabilities.update(selenoid_capabilities)
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
-
-    yield browser
-
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
