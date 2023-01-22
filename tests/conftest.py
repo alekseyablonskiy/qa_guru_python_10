e99e4f1bd874d326ab50e7200343c99363dfa05b
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2023-01-22 15:05:24 +0300

    python-dotenv added

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,7 +27,6 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
 
-
     driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
